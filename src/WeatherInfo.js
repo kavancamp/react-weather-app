ae562d3ee80fc6260ca9b3c6cd042a807b16392d
Keenah <kvancamp@mail.com>
2023-05-20 13:58:40 -0500

    Create WeatherInfo.js

---
+++ src/WeatherInfo.js
@@ -0,0 +1,28 @@
+import React from "react";
+import WeatherIcon from "./WeatherIcon";
+
+
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <div className="row">
+        <div className="col-6">
+          <div className="clearfix icon">
+            <WeatherIcon code={props.data.icon} size={60} />
+          </div>
+        </div>
+        <div className="float-left">
+          
+        </div>
+        <div className="col-6">
+          <ul>
+            <li className="description">{props.data.description}</li>
+            <li className="feels_like">Feels like: {props.data.feel}°</li>
+            <li className="humidity">Humidity: {props.data.humidity}%</li>
+            <li className="wind">Wind: {props.data.wind}km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
